233e0950d5648c9c01ef906156176a21f8be65a0
caigenkan <cai_genkan@outlook.com>
2020-09-22 17:25:56 +0800

    review DirectXMath chapters

--- ep01/xmvec3.cpp
+++ ep01/xmvec3.cpp
@@ -11,82 +11,91 @@ using namespace DirectX::PackedVector;
 // output XMVECTOR objects.
 ostream& XM_CALLCONV operator << (ostream& os, FXMVECTOR v)
 {
-    XMFLOAT3 dest;
-    XMStoreFloat3(&dest, v);
+	XMFLOAT3 dest;
+	XMStoreFloat3(&dest, v);
 
-    os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
-    return os;
+	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	return os;
 }
 
 int main()
 {
-    XMVECTOR test = XMVectorZero();
+	XMVECTOR test = XMVectorZero();
 
-    cout.setf(ios_base::boolalpha);
+	cout.setf(ios_base::boolalpha);
 
-    // Check support for SSE2 (Pentium4, AMD K8, and above).
-    if (!XMVerifyCPUSupport())
-    {
-        cout << "directx math not supported" << endl;
-        return 0;
-    }
+	// Check support for SSE2 (Pentium4, AMD K8, and above).
+	// 可进行 XMVECTOR 的四位数并行运算
+	if (!XMVerifyCPUSupport())
+	{
+		cout << "directx math not supported" << endl;
+		return 0;
+	}
 
-    XMVECTOR n = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-    XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
-    XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 0.0f);
-    XMVECTOR w = XMVectorSet(0.707f, 0.707f, 0.0f, 0.0f);
+	XMVECTOR n = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+	XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 0.0f);
+	XMVECTOR w = XMVectorSet(0.707f, 0.707f, 0.0f, 0.0f);
 
-    // Vector addition: XMVECTOR operator + 
-    XMVECTOR a = u + v;
+	// Vector addition: XMVECTOR operator + 
+	XMVECTOR a = u + v;
 
-    // Vector subtraction: XMVECTOR operator - 
-    XMVECTOR b = u - v;
+	// Vector subtraction: XMVECTOR operator - 
+	XMVECTOR b = u - v;
 
-    // Scalar multiplication: XMVECTOR operator * 
-    XMVECTOR c = 10.0f*u;
+	// Scalar multiplication: XMVECTOR operator * 
+	XMVECTOR c = 10.0f * u;
 
-    // ||u||
-    XMVECTOR L = XMVector3Length(u);
+	// ||u||
+	XMVECTOR L = XMVector3Length(u);
 
-    // d = u / ||u||
-    XMVECTOR d = XMVector3Normalize(u);
+	// d = u / ||u||
+	XMVECTOR d = XMVector3Normalize(u);
 
-    // s = u dot v
-    XMVECTOR s = XMVector3Dot(u, v);
+	// s = u dot v
+	XMVECTOR s = XMVector3Dot(u, v);
 
-    // e = u x v
-    XMVECTOR e = XMVector3Cross(u, v);
+	// e = u x v
+	XMVECTOR e = XMVector3Cross(u, v);
 
-    // Find proj_n(w) and perp_n(w)
-    XMVECTOR projW;
-    XMVECTOR perpW;
-    XMVector3ComponentsFromNormal(&projW, &perpW, w, n);
+	// Find proj_n(w) and perp_n(w)
+	XMVECTOR projW;     // 表示该向量在某个方向上的投影
+	XMVECTOR perpW;     // 标识基于某方向投影的正交
+	// 计算并传回 w 向量的正交化结果
+	// projW 为 w 在 n 方向上的投影
+	// perpW 为 w - n 且与 projW 正交的向量
+	// 没有对计算结果进行归一化(标准化)
+	XMVector3ComponentsFromNormal(&projW, &perpW, w, n);
 
-    // Does projW + perpW == w?
-    bool equal = XMVector3Equal(projW + perpW, w) != 0;
-    bool notEqual = XMVector3NotEqual(projW + perpW, w) != 0;
+	// Does projW + perpW == w?
+	// DirectXMath 库提供判断浮点数是否相等的方法 XMVector3NearEqual()
+	// 传入参数 FXMVECTOR FXMVECTOR Epsilon ， Epsilon 为可允许误差范围
+	bool equal = XMVector3Equal(projW + perpW, w) != 0;
+	bool notEqual = XMVector3NotEqual(projW + perpW, w) != 0;
 
-    // The angle between projW and perpW should be 90 degrees.
-    XMVECTOR angleVec = XMVector3AngleBetweenVectors(projW, perpW);
-    float angleRadians = XMVectorGetX(angleVec);
-    float angleDegrees = XMConvertToDegrees(angleRadians);
+	// The angle between projW and perpW should be 90 degrees.
+	// 使用 XMVECTOR 可以并行计算的优越性进行高效率的计算，而非使用 XMFLOAT3 等形式
+	// 计算完成后再将结果取出，能提升的效果未知
+	XMVECTOR angleVec = XMVector3AngleBetweenVectors(projW, perpW);
+	float angleRadians = XMVectorGetX(angleVec);
+	float angleDegrees = XMConvertToDegrees(angleRadians);
 
-    cout << "u                   = " << u << endl;
-    cout << "v                   = " << v << endl;
-    cout << "w                   = " << w << endl;
-    cout << "n                   = " << n << endl;
-    cout << "a = u + v           = " << a << endl;
-    cout << "b = u - v           = " << b << endl;
-    cout << "c = 10 * u          = " << c << endl;
-    cout << "d = u / ||u||       = " << d << endl;
-    cout << "e = u x v           = " << e << endl;
-    cout << "L  = ||u||          = " << L << endl;
-    cout << "s = u.v             = " << s << endl;
-    cout << "projW               = " << projW << endl;
-    cout << "perpW               = " << perpW << endl;
-    cout << "projW + perpW == w  = " << equal << endl;
-    cout << "projW + perpW != w  = " << notEqual << endl;
-    cout << "angle               = " << angleDegrees << endl;
+	cout << "u                   = " << u << endl;
+	cout << "v                   = " << v << endl;
+	cout << "w                   = " << w << endl;
+	cout << "n                   = " << n << endl;
+	cout << "a = u + v           = " << a << endl;
+	cout << "b = u - v           = " << b << endl;
+	cout << "c = 10 * u          = " << c << endl;
+	cout << "d = u / ||u||       = " << d << endl;
+	cout << "e = u x v           = " << e << endl;
+	cout << "L  = ||u||          = " << L << endl;
+	cout << "s = u.v             = " << s << endl;
+	cout << "projW               = " << projW << endl;
+	cout << "perpW               = " << perpW << endl;
+	cout << "projW + perpW == w  = " << equal << endl;
+	cout << "projW + perpW != w  = " << notEqual << endl;
+	cout << "angle               = " << angleDegrees << endl;
 
-    return 0;
+	return 0;
 }
--- ep02/xmmatrix.cpp
+++ ep02/xmmatrix.cpp
@@ -39,18 +39,25 @@ int main()
 		return 0;
 	}
 
+	// XMMATRIX 使用四个 XMVECTOR 实现，以此实现快速运算
+	// 通过指定4个 XMVECTOR 进行 XMMATRIX 的初始化(看上去是自动分配)
+	// 存储数据成员则使用 XMFLOAT4x4
 	XMMATRIX A(1.0f, 0.0f, 0.0f, 0.0f,
 		0.0f, 2.0f, 0.0f, 0.0f,
 		0.0f, 0.0f, 4.0f, 0.0f,
 		1.0f, 2.0f, 3.0f, 1.0f);
 
+	// 返回单位矩阵
 	XMMATRIX B = XMMatrixIdentity();
 
 	XMMATRIX C = A * B;
 
+	// 进行矩阵转置
 	XMMATRIX D = XMMatrixTranspose(A);
 
+	// 返回 ( det A, det A, det A, det A )，及4x1的行列式矩阵
 	XMVECTOR det = XMMatrixDeterminant(A);
+	// 返回 A 的逆矩阵
 	XMMATRIX E = XMMatrixInverse(&det, A);
 
 	XMMATRIX F = A * E;
@@ -62,6 +69,88 @@ int main()
 	cout << "det = determinant(A) = " << det << endl << endl;
 	cout << "E = inverse(A) = " << endl << E << endl;
 	cout << "F = A*E = " << endl << F << endl;
+
+	/// 第三章的矩阵组合变换记录于此
+
+	/// <summary>
+	/// 提供各方向轴上的缩放系数
+	/// </summary>
+	/// <returns>构建一个缩放矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixScaling(
+		float ScaleX, float ScaleY, float ScaleZ
+	);*/
+
+	/// <summary>
+	/// 以 XMVECTOR 的方式提供各方向轴上的缩放系数
+	/// </summary>
+	/// <returns>以 XMVECTOR 的方式构建一个缩放矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixScalingFromVector(
+		FXMVECTOR Scale
+	);*/
+
+	/// <summary>
+	/// 以顺时针方向按弧度 Angle 绕 x 轴进行旋转
+	/// </summary>
+	/// <returns>构建一个绕 x 轴的旋转矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixRotationX(
+		float Angle
+	);*/
+
+	/// <summary>
+	/// 以顺时针方向按弧度 Angle 绕 y 轴进行旋转
+	/// </summary>
+	/// <returns>构建一个绕 y 轴的旋转矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixRotationY(
+		float Angle
+	);*/
+
+	/// <summary>
+	/// 以顺时针方向按弧度 Angle 绕 z 轴进行旋转
+	/// </summary>
+	/// <returns>构建一个绕 z 轴的旋转矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixRotationZ(
+		float Angle
+	);*/
+
+	/// <summary>
+	/// 从 Axiz 轴的正方向看，以顺时针方向按弧度 Angle 进行旋转
+	/// </summary>
+	/// <returns>构建一个绕任意轴的旋转矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixRotationAxiz(
+		FXMVECTOR Axiz, float Angle
+	);*/
+
+	/// <summary>
+	/// 提供各方向轴上的平移系数
+	/// </summary>
+	/// <returns>构建一个平移矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixTranslation(
+		float OffsetX, float OffsetY, float OffsetZ
+	);*/
+
+	/// <summary>
+	/// 以 XMVECTOR 的方式提供各方向轴上的平移系数
+	/// </summary>
+	/// <returns>以 XMVECTOR 的方式构建一个平移矩阵并返回</returns>
+	/*XMMATRIX XM_CALLCONV XMMatrixTranslationFromVector(
+		FXMVECTOR Offset
+	);*/
+
+	/// <summary>
+	/// 计算向量v和矩阵M的乘积，针对点变化故 v 的齐次坐标为1，无需显式传参
+	/// </summary>
+	/// <returns>针对点变换返回 v*M</returns>
+	/*XMVECTOR XM_CALLCONV XMVector3TransformCoord(
+		FXMVECTOR V, CXMMATRIX M
+	);*/
+
+	/// <summary>
+	/// 计算向量v和矩阵M的乘积，针对向量变化故 v 的齐次坐标为0，无需显式传参
+	/// </summary>
+	/// <returns>针对向量变换返回 v*M</returns>
+	/*XMVECTOR XM_CALLCONV XMVector3TransformNormal(
+		FXMVECTOR V, CXMMATRIX M
+	);*/
 
 	return 0;
 }
